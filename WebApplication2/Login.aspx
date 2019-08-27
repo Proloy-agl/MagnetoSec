91f0df42fedfb4aced3d0da6bfab16fd0c723730
Proloy Mukherjee <A30003100@agl.com.au>
2019-08-28 01:47:33 +1000

    commit

--- WebApplication2/Login.aspx
+++ WebApplication2/Login.aspx
@@ -9,7 +9,9 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+    <asp:Button ID="Login" runat="server" Text="Login" OnClick="Login_Click" />
+    <asp:Button ID="Loginout" runat="server" Text="Logout"  OnClick="Loginout_Click"  />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
     </div>
     </form>
 </body>
--- WebApplication2/Login.aspx.cs
+++ WebApplication2/Login.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,25 @@ namespace WebApplication2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.IsAuthenticated)
+            {
+                Label1.Text = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("name").Value;
+            }
+        }
 
+        protected void Login_Click(object sender, EventArgs e)
+        {
+
+            Context.GetOwinContext().Authentication.Challenge(
+    new AuthenticationProperties { RedirectUri = "/" },
+    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+        }
+
+        protected void Loginout_Click(object sender, EventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut(
+               OpenIdConnectAuthenticationDefaults.AuthenticationType,
+               CookieAuthenticationDefaults.AuthenticationType);
         }
     }
 }
